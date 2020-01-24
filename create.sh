32fc1aae1ac673cfe160f17c7897a4e2d1ad37e7
e12exp <pklenze@ph.tum.de>
2020-01-24 15:03:32 +0100

    keep old version by default

--- create.sh
+++ create.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-
+echo "Running create.sh to create config/febex.db from text files"
 DB=febex.db
 cd $(dirname $0)
 
@@ -23,5 +22,25 @@ git branch | grep $HOSTNAME || git checkout -b $HOSTNAME
 git checkout $HOSTNAME 2>/dev/null
 git add *.h
 MSG="create.sh: created $DB on $HOSTNAME with modules=[$CONFIG]"
-echo "commit message: $MSG"
+#echo "commit message: $MSG"
 git commit -m "$MSG"
+
+if ! diff febex.db ../febex.db 
+then
+    if test "$1" != "--update"
+    then
+	echo "*******************************************************************"
+	echo "Warning, the newly created mbs/config/febex.db is different from"
+	echo " mbs/febex.db with the following creation date:"
+	ls --full-time ../febex.db
+	echo "Please consider updating to the the version."
+	echo "*******************************************************************"
+    else
+	echo "*******************************************************************"
+	echo "Warning, replacing febex.db with a newly created version"
+	echo "*******************************************************************"
+	cp febex.db ..
+    fi
+else
+    echo "mbs/febex.db is up to date with text files."
+fi
