b14aa343fcf6a4ec9cf64ee9de883c7973b4a101
land <land@lx-pool.gsi.de>
2022-01-05 20:32:34 +0100

    create.sh: created febex.db on x86l-75 with modules=[17, 17, 17, 18 ]. UPDATED mbs/febex.db. (replacing commit 7ef1c167978b7155e3abd41d7f7147dadde2588a)

--- febex.h
+++ febex.h
@@ -5,13 +5,19 @@
 add sfp 4
 
 // write your modules here:
-add module 0 16
-add module 1 16
-add module 2 16
-add module 3 16
+add module 0 17
+add module 1 17
+add module 2 17
+add module 3 18
+
 
 #include "default/base.h" // base config for febex 
 #include "thresholds.h"
 set *.*.*.trigger_validation_src 0x100
 set *.*.*.discr_threshold_timing 50
 set *.*.*.discr_threshold_gamma 100
+
+// last modules are not to be be read out
+
+set 0-2.16.*.opmode_run 0
+set 3.17.*.opmode_run 0
